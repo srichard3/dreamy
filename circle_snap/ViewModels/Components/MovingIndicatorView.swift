68ca2b80fd4c193d3f7215444433586751dba278
Duy Nguyen <duybnguyen04@gmail.com>
2024-11-05 14:55:24 -0500

    redesign to a bar indicator again

--- circle_snap/ViewModels/Components/MovingIndicatorView.swift
+++ circle_snap/ViewModels/Components/MovingIndicatorView.swift
@@ -8,14 +8,50 @@ import SwiftUI
 
 struct MovingIndicatorView: View {
     let progress: Double
+    let circleRadius: CGFloat = GameConstants.circleRadius
     
     var body: some View {
-        Rectangle()
-            .frame(width: 20, height: 20)
-            .foregroundColor(.blue)
-            .offset(
-                x: GameConstants.circleRadius * cos((progress * 360) * .pi / 180),
-                y: GameConstants.circleRadius * sin((progress * 360) * .pi / 180)
-            )
+        GeometryReader { geometry in
+            ZStack {
+                IndicatorShape(progress: progress, circleRadius: circleRadius)
+                    .fill(LinearGradient(
+                        gradient: Gradient(colors: [.blue, .purple]),
+                        startPoint: .leading,
+                        endPoint: .trailing
+                    ))
+                    .frame(width: 10, height: 50)
+                    .rotationEffect(Angle(degrees: (progress * 360) - 90))
+                    .position(
+                        x: geometry.size.width / 2 + circleRadius * cos((progress * 360) * .pi / 180),
+                        y: geometry.size.height / 2 + circleRadius * sin((progress * 360) * .pi / 180)
+                    )
+                    .cornerRadius(50)
+            }
+        }
+    }
+}
+
+struct IndicatorShape: Shape {
+    let progress: Double
+    let circleRadius: CGFloat
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        let width: CGFloat = 7
+        let height: CGFloat = 42
+        let cornerRadius: CGFloat = 5
+        
+        let startX = rect.midX - width / 2
+        let startY = rect.midY - height / 2
+        
+        path.move(to: CGPoint(x: startX, y: startY))
+        path.addLine(to: CGPoint(x: startX + width, y: startY))
+        path.addLine(to: CGPoint(x: startX + width, y: startY + height))
+        path.addLine(to: CGPoint(x: startX, y: startY + height))
+        path.addLine(to: CGPoint(x: startX, y: startY))
+        path.addRoundedRect(in: CGRect(x: startX, y: startY, width: width, height: height), cornerSize: CGSize(width: cornerRadius, height: cornerRadius))
+        
+        return path
     }
 }
