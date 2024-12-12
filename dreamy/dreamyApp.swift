2d8bc124176068244bcb85381e5a75899fd4f4c9
Samuel Richard <sam@hyel.app>
2024-12-11 18:38:50 -0800

    Initial Commit

---
+++ dreamy/dreamyApp.swift
@@ -0,0 +1,17 @@
+//
+//  dreamyApp.swift
+//  dreamy
+//
+//  Created by Sam Richard on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct dreamyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
